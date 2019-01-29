b522f93de58418dd9e0a01d8d8872de94597f9cb
Tijl Van den Brugghen <me@tijlvdb.me>
2019-01-29 23:46:07 +0100

    [INTERFACE] Remove unneeded lib vue-select (replaced by vue-multiselect)

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,6 @@
 import Vue from 'vue/dist/vue.js'
 import VueRouter from 'vue-router'
 import VueProgress from 'vue-progress-path'
-import VueSelect from 'vue-select'
 
 import main from './main.vue'
 import routes from './routes'
