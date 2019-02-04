f16811c7b9f11a67987d530468a411f4dea11fa3
Tijl Van den Brugghen <me@tijlvdb.me>
2019-02-04 21:15:31 +0100

    [INTERFACE] Get rid of the dependency to pug (there was only one file anyway)

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,5 +1,11 @@
+const { resolve } = require('path')
+const { createReadStream } = require('fs')
+
 module.exports = (s) => {
+    let file = resolve(__dirname, '..', '..', 'public', 'index.html')
+
     s.router.get('/', async (ctx, next) => {
-        await ctx.render('index.pug')
+        ctx.type = 'html'
+        ctx.body = createReadStream(file)
     })
 }
--- app/server.js
+++ app/server.js
@@ -2,7 +2,6 @@ const Koa = require('koa')
 const Router = require('koa-router')
 const static = require('koa-static')
 const mount = require('koa-mount')
-const views = require('koa-views')
 const body = require('koa-body')
 const chalk = require('chalk')
 const dateFormat = require('dateformat')
@@ -11,10 +10,6 @@ let app = new Koa()
 let router = new Router()
 
 app.use(mount('/public', static(__dirname + '/../public/')))
-
-app.use(views(__dirname + '/views'), {
-    extension: 'pug'
-})
 
 app.use(body())
 
